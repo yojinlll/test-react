753c338a274c22ce705edc02012914e8ca45db2a
yojin <yojin1995@qq.com>
2020-01-31 13:50:50 +0800

    webpack config 环境区分, index.d.ts 移至 dist

--- lib/index.tsx
+++ lib/index.tsx
@@ -2,4 +2,6 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import Button from './button'
 
+const test = 'test'
+console.log(test);
 ReactDom.render(<Button></Button>, document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: 'production',
+  // mode: 'production',
   entry: {
-    'miro-react': './lib/index.tsx'
+    index: './lib/index.tsx'
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx']
@@ -21,25 +21,5 @@ module.exports = {
         loader: 'awesome-typescript-loader'
       }
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'fuck-webpack',
-      template: 'index.html',
-    })
-  ],
-  externals:{
-    react: {
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'react',
-      root: 'react',
-    },
-    'react-dom': {
-      commonjs: 'react-dom',
-      commonjs2: 'react-dom',
-      amd: 'react-dom',
-      root: 'react-dom',
-    },
   }
 }
