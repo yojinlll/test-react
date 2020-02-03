7361917a956a01af5d01d9d173d9c5738c5e7d9d
yojin <yojin1995@qq.com>
2020-02-03 13:17:46 +0800

    onClick 类型完善

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import './importIcons'
 import './icons.scss'
 
-interface IconProps{
-  name?: string;
+interface IconProps {
+  name: string;
+  onClick?: React.MouseEventHandler<SVGElement>
 }
 
-const Icon:React.FunctionComponent<IconProps> = (props)=>{
+const Icon: React.FunctionComponent<IconProps> = (props) => {
   return (
-    <svg className='miro-icons'>
+    <svg className='miro-icons' onClick={props.onClick}>
       <use xlinkHref={`#${props.name}`}></use>
     </svg>
   )
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Icon from './icon'
 
+const fn:React.MouseEventHandler = (e)=>{
+  console.log('fn', e.target);
+}
+
 ReactDOM.render(<div>
-  <Icon name='github'/>
+  <Icon name='github' onClick={fn}/>
 </div>, document.getElementById('root'))
