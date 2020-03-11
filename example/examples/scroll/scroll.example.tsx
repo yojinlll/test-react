b1210d60bca015e5ed2b05ef704878e897766d95
yojin <yojin1995@qq.com>
2020-03-11 23:45:43 +0800

    feat: scroll

--- example/examples/scroll/scroll.example.tsx
+++ example/examples/scroll/scroll.example.tsx
@@ -4,9 +4,9 @@ import Scroll from '../../../lib/scroll/scroll'
 const ScrollExample: React.FunctionComponent = () => {
   return (
     <div>
-      <div>第一个例子</div>
+      <div>默认</div>
 
-      <Scroll style={{ height: 400, border: '1px solid red', margin: '40px'}}>
+      <Scroll style={{ height: 400, border: '1px solid #dd9a9a', margin: '40px'}}>
         <p>1</p>
         <p>2</p>
         <p>3</p>
@@ -27,6 +27,33 @@ const ScrollExample: React.FunctionComponent = () => {
         <p>18</p>
         <p>19</p>
         <p>20</p>
+      </Scroll>
+
+      <div>触发滚动后出现滚动条</div>
+
+      <Scroll always={false} style={{ height: 400, border: '1px solid #dd9a9a', margin: '40px'}}>
+        <div>
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+          <p>4</p>
+          <p>5</p>
+          <p>6</p>
+          <p>7</p>
+          <p>8</p>
+          <p>9</p>
+          <p>10</p>
+          <p>11</p>
+          <p>12</p>
+          <p>13</p>
+          <p>14</p>
+          <p>15</p>
+          <p>16</p>
+          <p>17</p>
+          <p>18</p>
+          <p>19</p>
+          <p>20</p>
+        </div>
       </Scroll>
     </div>
   )
--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,20 +1,57 @@
-import React, { HtmlHTMLAttributes } from 'react'
+import React, { HtmlHTMLAttributes, UIEventHandler, useState, useEffect, useRef } from 'react'
 import './scroll.scss'
 import scrollbarWidth from './scrollbar-width';
 
 interface Props extends HtmlHTMLAttributes<HTMLElement> {
-
+  always?: Boolean
 }
 
 const Scroll: React.FunctionComponent<Props> = (props) => {
-  const { children, ...reset } = props
+  const { children, always, ...reset } = props
+  const [barHeight, setBarHeight] = useState(0)
+  const [barTop, setBarTop] = useState(0)
+  const [opacity, setOpacity] = useState(0)
+  const onscroll:UIEventHandler = (e)=>{
+    const { current } = containerRef
+    const scrollHeight = current!.scrollHeight
+    const viewHeight = current!.getBoundingClientRect().height
+    const scrollTop = current!.scrollTop
+    setBarTop( scrollTop * viewHeight / scrollHeight )
+    setOpacity( scrollTop ? 1 : 0 )
+  }
+
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  // 生命周期
+  useEffect(() => {
+    const scrollHeight = containerRef.current!.scrollHeight
+    const viewHeight = containerRef.current!.getBoundingClientRect().height
+    setBarHeight( viewHeight * viewHeight / scrollHeight )
+  }, [])
+
   return (
     <div className='miro-scroll' {...reset}>
-      <div className='miro-scroll-inner' style={{ right: -scrollbarWidth() }}>
+      <div
+        ref={ containerRef }
+        className='miro-scroll-inner'
+        style={{ right: -scrollbarWidth() }}
+        onScroll={onscroll}>
         { children }
+      </div>
+      <div
+        className="miro-scroll-track"
+        style={{
+          opacity: always ? 1 : opacity,
+        }}
+      >
+        <div className="miro-scroll-bar" style={{ height: barHeight, transform: `translateY(${barTop}px)` }} />
       </div>
     </div>
   )
+}
+
+Scroll.defaultProps = {
+  always: true
 }
 
 export default Scroll
